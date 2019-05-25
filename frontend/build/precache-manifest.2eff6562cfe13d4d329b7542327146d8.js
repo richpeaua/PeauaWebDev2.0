3b49ce8d69455ee781225b7818f44cf2412b20d2
Rich <lpeaua@gmail.com>
2019-05-24 23:22:36 -0700

    adding images for social media links

--- frontend/build/precache-manifest.2eff6562cfe13d4d329b7542327146d8.js
+++ frontend/build/precache-manifest.2eff6562cfe13d4d329b7542327146d8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1798208123ecd3b5439996725cca084",
+    "revision": "d2e20146d8ef1ccd8958f258eebe237e",
     "url": "/index.html"
   },
   {
-    "revision": "c1a41ad759e5be92e7ec",
+    "revision": "715b5940a0ccba01623c",
     "url": "/static/css/main.97c14f1c.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.fe9850f3.chunk.js"
   },
   {
-    "revision": "c1a41ad759e5be92e7ec",
-    "url": "/static/js/main.1370bd13.chunk.js"
+    "revision": "715b5940a0ccba01623c",
+    "url": "/static/js/main.db1297fb.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92bb2f4493269aad2f0418dba5f86a9b.js"
+  "/precache-manifest.2eff6562cfe13d4d329b7542327146d8.js"
 );
 
 self.addEventListener('message', (event) => {
