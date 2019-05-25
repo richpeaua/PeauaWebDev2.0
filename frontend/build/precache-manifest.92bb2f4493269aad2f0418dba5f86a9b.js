2df1b764d3db7c33562df23e7f59cd311d17a38b
Rich <lpeaua@gmail.com>
2019-05-24 22:25:38 -0700

    build

---
+++ frontend/build/precache-manifest.92bb2f4493269aad2f0418dba5f86a9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1798208123ecd3b5439996725cca084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1a41ad759e5be92e7ec",
+    "url": "/static/css/main.97c14f1c.chunk.css"
+  },
+  {
+    "revision": "d8208e187208f1e4c298",
+    "url": "/static/js/2.fe9850f3.chunk.js"
+  },
+  {
+    "revision": "c1a41ad759e5be92e7ec",
+    "url": "/static/js/main.1370bd13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
