cab24fc9864d74a2c04c88e4bfe47ebc2e49910f
Rich <lpeaua@gmail.com>
2019-05-29 04:13:38 -0700

    social media links fixed

--- frontend/src/components/SocialMedia.js
+++ frontend/src/components/SocialMedia.js
@@ -5,9 +5,9 @@ const SocialMedia = () => {
 	return (
 	  <div className='social-media-roll'>
 	    <ul>
-	      <li className='social-media-link'><Link to="https://www.linkedin.com/in/rich-peaua-004492125/" ><img className='social-media-img' alt='linkedIn' src={ require('../assets/images/icons/linkdin-blue.png') }/></Link></li>
-	      <li className='social-media-link'><Link to="https://twitter.com/peauaio" ><img className='social-media-img' alt='github' src={ require('../assets/images/icons/git-hub-blue.png') }/></Link></li>
-	      <li className='social-media-link'><Link to="https://github.com/lpeaua/PeauaWebDev" ><img className='social-media-img' alt='twitter' src={ require('../assets/images/icons/twitter-blue.png') } /></Link></li>
+	      <li className='social-media-link'><a href="https://www.linkedin.com/in/rich-peaua-004492125/" target='_blank'><img className='social-media-img' alt='linkedIn' src={ require('../assets/images/icons/linkdin-blue.png') }/></a></li>
+	      <li className='social-media-link'><a href="https://github.com/lpeaua/PeauaWebDev" target='_blank'><img className='social-media-img' alt='github' src={ require('../assets/images/icons/git-hub-blue.png') }/></a></li>
+	      <li className='social-media-link'><a href="https://twitter.com/peauaio" target='_blank'><img className='social-media-img' alt='twitter' src={ require('../assets/images/icons/twitter-blue.png') } /></a></li>
 	    </ul>
 	  </div>
 	);
