1f94c9b5b115998daec16b59b9cb494b61ae24f2
root <root@peaua.io>
2019-05-31 08:17:26 +0000

    containerizing frontend

---
+++ frontend/build/precache-manifest.aa7abee43d2e2ce824dbd6dff2a0338c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5cfad8684b7a5af762fb0a60c10184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b5acf3e2dc5746b2803",
+    "url": "/static/css/main.48ecb96d.chunk.css"
+  },
+  {
+    "revision": "d8208e187208f1e4c298",
+    "url": "/static/js/2.fe9850f3.chunk.js"
+  },
+  {
+    "revision": "0b5acf3e2dc5746b2803",
+    "url": "/static/js/main.d203cb9d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6b14999458e9dd430aa9e37dd8d6765",
+    "url": "/static/media/git-hub-blue.f6b14999.png"
+  }
+]);
