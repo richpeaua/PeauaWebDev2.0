d9be6e02961178da6d0e1bacb369ad65efb72ffb
root <root@peaua.io>
2019-06-04 10:44:46 +0000

    fixing docker configs and structure

--- backend/start.sh
+++ backend/start.sh
@@ -6,7 +6,7 @@ python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="db")
+    conn = psycopg2.connect(dbname="peaua_db", user="peauaio", password="password", host="db")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
@@ -19,9 +19,11 @@ until postgres_ready; do
 done
 
 python manage.py makemigrations blog
+python manage.py makemigrations
+python manage.py migrate blog
 python manage.py migrate
 
 echo Starting Gunicorn.
 exec gunicorn backend.wsgi \
     --bind 0.0.0.0:8000 \
-    --workers 3
+    --workers 2
